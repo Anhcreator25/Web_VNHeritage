a2525df39f06cf4a9fd43e694551b62fbd0ef111
nhn0102 <nguyenhoanam01022007@gmail.com>
2026-05-12 13:59:23 +0700

    Create articleData.js

---
+++ JS/articleData.js
@@ -0,0 +1,70 @@
+const articleData = {
+
+    hoian: {
+        title: "Khám phá vẻ đẹp cổ kính của Phố cổ Hội An",
+        image: "image/hoian.jpg",
+        category: "Văn hóa",
+        date: "07/05/2026",
+        views: "2.5k",
+
+        content: `
+            <p>Hội An là một trong những di sản nổi tiếng nhất Việt Nam.</p>
+
+            <p>
+                Những ngôi nhà vàng cổ kính, đèn lồng rực rỡ
+                và dòng sông Hoài thơ mộng tạo nên vẻ đẹp đặc trưng.
+            </p>
+
+            <p>
+                Hội An từng là thương cảng quốc tế sầm uất vào thế kỷ 16.
+            </p>
+        `
+    },
+
+    halong: {
+
+        title: "5 trải nghiệm không thể bỏ lỡ tại Vịnh Hạ Long",
+
+        image: "image/vhl.jpg",
+
+        category: "Thiên nhiên",
+
+        date: "06/05/2026",
+
+        views: "1.9k",
+
+        content: `
+            <p>
+                Vịnh Hạ Long nổi tiếng với hàng nghìn đảo đá vôi tuyệt đẹp.
+            </p>
+
+            <p>
+                Đây là một trong những kỳ quan thiên nhiên nổi tiếng nhất thế giới.
+            </p>
+        `
+    },
+
+    hue: {
+
+        title: "Những nét kiến trúc độc đáo của Cố đô Huế",
+
+        image: "image/hue.jpg",
+
+        category: "Kiến trúc",
+
+        date: "05/05/2026",
+
+        views: "1.3k",
+
+        content: `
+            <p>
+                Huế mang đậm dấu ấn triều Nguyễn với các công trình cung đình cổ kính.
+            </p>
+
+            <p>
+                Đây là trung tâm văn hóa lịch sử đặc biệt của Việt Nam.
+            </p>
+        `
+    }
+
+}
